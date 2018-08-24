eff73015e9f6c9986d72a626527d9c4258ce613b
alexander wagstaff <awagstaff6@gmail.com>
2018-08-24 13:07:46 -0500

    initial commit

---
+++ bare_metal/main.tf
@@ -0,0 +1,119 @@
+provider "oci" {
+  tenancy_ocid     = "${var.tenancy_ocid}"
+  user_ocid        = "${var.user_ocid}"
+  fingerprint      = "${var.fingerprint}"
+  private_key_path = "${var.private_key_path}"
+  region           = "${var.region}"
+}
+
+data "oci_identity_availability_domains" "ADs" {
+  compartment_id = "${var.tenancy_ocid}"
+}
+
+resource "oci_core_virtual_network" "bare_metal_vcn" {
+  cidr_block     = "10.0.0.0/16"
+  compartment_id = "${var.compartment_ocid}"
+  display_name   = "Baremetal VCN"
+  dns_label      = "bare_oci_core_virtual_network.bare_metal_vcn"
+}
+
+resource "oci_core_subnet" "bare_metal_subnet" {
+  availability_domain = "${lookup(data.oci_identity_availability_domains.ADs.availability_domains[var.AD - 1],"name")}"
+  cidr_block          = "10.0.0.0/24"
+  display_name        = "Baremetal Subnet"
+  compartment_id      = "${var.compartment_ocid}"
+  vcn_id              = "${oci_core_virtual_network.bare_metal_vcn.id}"
+  route_table_id      = "${oci_core_route_table.bare_metal_route_table.id}"
+  security_list_ids   = ["${oci_core_virtual_network.bare_metal_vcn.default_security_list_id}"]
+  dhcp_options_id     = "${oci_core_virtual_network.bare_metal_vcn.default_dhcp_options_id}"
+  dns_label           = "baremetalsubnet"
+}
+
+resource "oci_core_subnet" "hyperv_subnet" {
+  availability_domain = "${lookup(data.oci_identity_availability_domains.ADs.availability_domains[var.AD - 1],"name")}"
+  cidr_block          = "10.0.1.0/24"
+  display_name        = "Hyper-V Subnet"
+  compartment_id      = "${var.compartment_ocid}"
+  vcn_id              = "${oci_core_virtual_network.bare_metal_vcn.id}"
+  route_table_id      = "${oci_core_route_table.bare_metal_route_table.id}"
+  security_list_ids   = ["${oci_core_virtual_network.bare_metal_vcn.default_security_list_id}"]
+  dhcp_options_id     = "${oci_core_virtual_network.bare_metal_vcn.default_dhcp_options_id}"
+  dns_label           = "hypervsubnet"
+}
+
+resource "oci_core_internet_gateway" "bare_metal_gateway" {
+  compartment_id = "${var.compartment_ocid}"
+  display_name   = "Baremetal Gateway"
+  vcn_id         = "${oci_core_virtual_network.bare_metal_vcn.id}"
+}
+
+resource "oci_core_route_table" "bare_metal_route_table" {
+  compartment_id = "${var.compartment_ocid}"
+  vcn_id         = "${oci_core_virtual_network.bare_metal_vcn.id}"
+  display_name   = "Baremetal Route Table"
+
+  route_rules {
+    destination       = "0.0.0.0/0"
+    network_entity_id = "${oci_core_internet_gateway.bare_metal_gateway.id}"
+  }
+
+  #   route_rules {
+  #     destination        = "192.168"
+  #     network_entity_id = "${oci_core_internet_gateway.baremetal_gateway.id}"
+  #   }
+}
+
+resource "oci_core_instance" "bm_instance" {
+  availability_domain = "${lookup(data.oci_identity_availability_domains.ADs.availability_domains[var.AD - 1],"name")}"
+  compartment_id      = "${var.compartment_ocid}"
+  display_name        = "BM Instance"
+  image               = "${var.instance_image_ocid[var.region]}"
+  shape               = "${var.instance_shape}"
+  subnet_id           = "${oci_core_subnet.bare_metal_subnet.id}"
+
+  create_vnic_details {
+    subnet_id        = "${oci_core_subnet.bare_metal_subnet.id}"
+    hostname_label   = "bminstancevnic"
+    display_name     = "BM Instance VNIC"
+    assign_public_ip = true
+  }
+
+  timeouts {
+    create = "60m"
+  }
+}
+
+resource "oci_core_vnic_attachment" "bm_vnic_2" {
+  instance_id = "${oci_core_instance.bm_instance.id}"
+  nic_index   = "1"
+
+  create_vnic_details {
+    subnet_id        = "${oci_core_subnet.hyperv_subnet.id}"
+    assign_public_ip = true
+    display_name     = "BM Instance VNIC 2"
+  }
+}
+
+resource "oci_core_vnic_attachment" "hvnat_vnic" {
+  instance_id = "${oci_core_instance.bm_instance.id}"
+  nic_index   = "1"
+
+  create_vnic_details {
+    subnet_id              = "${oci_core_subnet.hyperv_subnet.id}"
+    assign_public_ip       = true
+    display_name           = "hvnat vnic"
+    skip_source_dest_check = true
+  }
+}
+
+resource "oci_core_vnic_attachment" "hvrouter_vnic" {
+  instance_id = "${oci_core_instance.bm_instance.id}"
+  nic_index   = "1"
+
+  create_vnic_details {
+    subnet_id              = "${oci_core_subnet.hyperv_subnet.id}"
+    assign_public_ip       = true
+    display_name           = "hvrouter vnic"
+    skip_source_dest_check = true
+  }
+}
---
+++ bare_metal/vars.tf
@@ -0,0 +1,32 @@
+variable "compartment_ocid" {
+  description = "The ocid of the compartment to deploy in"
+}
+
+variable "tenancy_ocid" {
+  description = "The ocid of the tenancy to deploy in"
+}
+
+variable "user_ocid" {
+  description = "The ocid of the user"
+}
+
+variable "region" {
+  description = "The region to deploy in"
+}
+
+variable "fingerprint" {
+  description = "The fingerprint of the key to use"
+}
+
+variable "instance_image_ocid" {
+  type = "map"
+
+  default = {
+    eu-frankfurt-1 = "ocid1.image.oc1.eu-frankfurt-1.aaaaaaaaazomv4stgxzsvknokxv2dprak6q7jugzzozpqpjfuiuxks2afkfq"
+    us-ashburn-1   = "ocid1.image.oc1.iad.aaaaaaaatach4vk7fk7wgwocr7fjunzwagz3bygj4mc7axajdokfwnuwsibq"
+    us-phoenix-1   = "ocid1.image.oc1.phx.aaaaaaaa22s7v5slgeqohhu2qxaiiehd7q2mdu2y4ta2ggpuzyug3io3bcyq"
+    uk-london-1    = "ocid1.image.oc1.uk-london-1.aaaaaaaawbhbkek3gto5z4ugupavd42b5tsvi6qoxagpffr3jjnqihvvsila"
+  }
+
+  description = "A map of the images to use by region"
+}
